de59e64e0aba0d2589543c4cfa2eb09ab72a5312
Derek Henscheid <derek-henscheid@uiowa.edu>
2013-09-07 12:43:11 -0500

    second text problem done

--- js/Text/reverse.js
+++ js/Text/reverse.js
@@ -3,12 +3,12 @@
 var prompt = require('prompt');
 prompt.start();
 prompt.message = 'Reverse'
-prompt.get(['string'], function(err, result) {
-    var reversed = [],
-        input = result.string;
-    for (var i = input.length - 1; i >= 0; i--) {
-      reversed.push(input[i]);
-    }
+prompt.get(['text'], function(err, result) {
+  var reversed = [],
+      input = result.text;
+  for (var i = input.length - 1; i >= 0; i--) {
+    reversed.push(input[i]);
+  }
     
-    console.log('%s reversed is: %s', input, reversed.join(''))
-  });
+  console.log('%s reversed is:\n%s', input, reversed.join(''))
+});
